0ee600768ac587792b7e639b9c05ee9225c1bf81
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-22 16:39:46 -0800

    Removed deprecated code from cards and cards container

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,33 +1,6 @@
 import React from 'react';
 
-// class Card extends Component {
-//   render() {
-//     return (
-//       <div id={this.props.id}>
-//         <p>{this.props.itemName}</p>
-//       </div>
-//     );
-//   }
-// }
-
 const styleString = `background-image: url({props.info.photo})`;
-
-// const Card = (props) => {
-//   console.log('in cards for item', props.info.item_name)
-//   return (
-//     <div className="card" className="col-4">
-//     <div className="card-container">
-//         <div className="row">
-//             <img src={props.info.photo} className="level" id="projectlogo" />
-//             <p id="projectname" className="title level">{props.info.item_name}</p>
-//             <p>props.info.item_name</p>
-//             <p>{props.info.item_details}</p>
-//             <p>{props.info.created_at}</p>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// };
 
 const Card = props => {
   console.log('in cards for item', props.info.item_name);
--- client/containers/CardsContainer.jsx
+++ client/containers/CardsContainer.jsx
@@ -4,25 +4,6 @@ import Card from '../components/Card.jsx';
 
 const uuid = require('uuid/v1');
 
-// class CardsContainer extends Component {
-//   cardFunction(item) {
-//     return (
-//       <Card
-//         itemName={item.item_name}
-//         itemPrice={item.price}
-//         itemPhoto={item.photo}
-//         itemDetails={item.item_details}
-//         key={item.id}
-//       />
-//     );
-//   }
-
-//   render() {
-//     const cards = this.props.items.map(this.cardFunction, this);
-//     return <div id="cards-container">{cards}</div>;
-//   }
-// }
-
 const CardsContainer = props => {
   console.log('here are the props inside CardsContainer ', props);
 
@@ -33,33 +14,12 @@ const CardsContainer = props => {
 
   const cards = props.items.map(createCard);
 
-  // const cardRows = [];
-
-  // let counter = 0;
-
-  // for (let i = 0; i <= cards.length / 4; i++) {
-  //   const currentCards = [];
-
-  //   for (let j = 0; j < 4; j++) {
-  //     currentCards.push(cards[counter]);
-  //     counter++;
-  //   }
-
-  //   const currentRow = (
-  //     <div className="row" key={uuid()}>
-  //       {' '}
-  //       {currentCards}
-  //     </div>
-  //   );
-  //   cardRows.push(currentRow);
-  // }
-
   let Loading;
 
   if (props.fetchFlag) {
     Loading = (
       <div className="cardgrid">
-      {/* <div className="col-3"> */}
+        {/* <div className="col-3"> */}
         <button
           className="animated loading center loading-white loading-right white"
           id="loadButton"
@@ -71,31 +31,11 @@ const CardsContainer = props => {
   }
 
   return (
-    // <div className="cardgrid card-container col-4">
     <div className="cardgrid">
       {Loading}
-      {/* {cardRows} */}
       {cards}
     </div>
   );
 };
-// <Card
-//   itemName={item.item_name}
-//   itemPrice={item.price}
-//   itemPhoto={item.photo}
-//   itemDetails={item.item_details}
-//   key={item.id}
-// />
-// }
-
-// function checkRender(props) {
-//   console.log(props.items);
-//   if (typeof props.items === 'undefined') return null;
-//   return props.items.map(cardFunction);
-// }
-
-// function CardsContainer(props) {
-//   return <div id="cards-container">{checkRender(props)}</div>;
-// }
 
 export default CardsContainer;
